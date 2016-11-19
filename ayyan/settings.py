382cc69b5db681b204ae168ef887f1c32fd25aa0
Jagadesh Babu T <jagadeshbabut@gmail.com>
2016-11-19 12:34:32 +0530

    Updated settings to allow all hosts in local settings, removed postgres from docker configs and ignored editor configurations

--- ayyan/settings.py
+++ ayyan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u+gcgr#dpb#(1ti4shac2_90ndra+qfs=^t!=wsrp_@0a$xsk+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
